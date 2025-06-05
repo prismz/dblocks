312a39969ef4ca0482739f337d8c70ccc2c8dc22
Hasan Zahra <hasan@hasanzahra.xyz>
2025-06-04 20:09:52 -0500

    spotify script updates

--- elements/dblocks-spotify.sh
+++ elements/dblocks-spotify.sh
@@ -11,9 +11,21 @@ convert_secs() {
         printf "%d:%02d" "$((10#$1 / 60))" "$((10#$1 % 60))";
 }
 
-status=$(playerctl -p spotify status)
-artist=$(playerctl -p spotify metadata artist)
-title=$(playerctl -p spotify metadata title)
+status_text=$(playerctl -p spotify status)
+status=""
+case "$status_text" in
+        Playing)
+                status="▶️ " ;;
+        Paused)
+                status="⏸️ " ;;
+        *)
+                status=""
+esac
+
+artist=$(truncate 16 "$(playerctl -p spotify metadata artist)")
+title=$(truncate 16 "$(playerctl -p spotify metadata title)")
+vol_rounded=$(printf "%0.2f" "$(playerctl -p spotify volume)")
+volume=$(echo "scale=0; (100 * $vol_rounded)/1" | bc)
 
 position_raw=$(playerctl -p spotify position | sed 's/\./\n/g' | head -n 1)
 
@@ -23,7 +35,11 @@ position_raw=$(playerctl -p spotify position | sed 's/\./\n/g' | head -n 1)
 # UGLY
 length_raw=$(($(playerctl -p spotify metadata | grep length | sed 's/ /\n/g' | tail -n 1) / 1000000))
 
+track_progress_percent=$(echo "scale=2; $position_raw / $length_raw" | bc)
+track_progress_bar=$(progbar $track_progress_percent 20)
+
 position=$(convert_secs "$position_raw")
 length=$(convert_secs "$length_raw")
 
-echo "$status $artist - $title $position/$length"
+#echo "$track_progress_bar $status$artist - $title $position/$length ($volume%)"
+echo "$status$title - $artist $track_progress_bar (🔊 $volume%)"
